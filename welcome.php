3fdc70343caa253dc1b328644dd22515d268a8a9
Eddelito S. Namoc <94291380+ENAMOC@users.noreply.github.com>
2023-12-09 09:40:26 +0800

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="login.css">
+
+</head>
+<body>
+    
+    
+</body>
+</html>
